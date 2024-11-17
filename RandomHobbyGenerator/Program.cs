aad6cdf7ac183bc169b87d2b43e211ead7d9cf27
chrissundal <christoffersj@hotmail.com>
2024-11-17 10:34:45 +0100

    f

---
+++ RandomHobbyGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace RandomHobbyGenerator
+{
+    internal class Program
+    {
+        string[] hobbies = { "Stamp-collecting", "football-playing", "lord of the rings re-enactor", "dancing", "Kickboxing", "Golfing", "Swimming" };
+
+        string[] answers =
+            { "it looks like your new hobby is", "is it true your hobby are", "wow, your hobby are" };
+        Random rnd = new Random();
+        int number = 0;
+        public void Run()
+        {
+            while(number < hobbies.Length)
+            {
+                int AnswerIndex = rnd.Next(answers.Length);
+                int index = rnd.Next(hobbies.Length);
+                string randomHobby = hobbies[index];
+                string randomAnswer = answers[AnswerIndex];
+                Console.WriteLine("\nWho would like a new hobby?");
+                var firstAnswer = Console.ReadLine();
+                Console.WriteLine($"So {firstAnswer} {randomAnswer} {randomHobby}?");
+                number++;
+            }
+        }
+
+
+        public 
+        static void Main(string[] args)
+        {
+            Program runRandom = new Program();
+            runRandom.Run();
+        }
+    }
+}
---
+++ switch/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace switchOppgave;
+
+internal class Program
+{
+    string[] dayArray = { "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" };
+
+    public void Run()
+    {
+        int index = dayArray.Length;
+
+        Console.WriteLine("Hvilken dag er det?");
+        var input = Convert.ToInt32(Console.ReadLine());
+
+        //if (input >= 1 && input <= 7)
+        //{
+        //    Console.WriteLine($"Dagen er: {dayArray[input - 1]}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Ugyldig input. Skriv inn et nummer fra 1 til 7."); 
+        //    Run();
+        //}
+
+        switch (input)
+        {
+            case int day when day >= 1 && day <= 7:
+                Console.WriteLine($"Dagen er: {dayArray[input - 1]}");
+                break;
+            default:
+                Console.WriteLine("Ugyldig input. Skriv inn et nummer fra 1 til 7.");
+                Run();
+                break;
+        }
+
+        //switch (input)
+        //{
+        //    case 1:
+        //        Console.WriteLine($"Dagen er: {dayArray[0]}");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine($"Dagen er: {dayArray[1]}");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine($"Dagen er: {dayArray[2]}");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine($"Dagen er: {dayArray[3]}");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine($"Dagen er: {dayArray[4]}");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine($"Dagen er: {dayArray[5]}");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine($"Dagen er: {dayArray[6]}");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Ugyldig input. Skriv inn et nummer fra 1 til 7.");
+        //        Run();
+        //        break;
+        //}
+
+        //switch (input)
+                //{
+                //    case "1":
+                //        Console.WriteLine("Mandag");
+                //        break;
+                //    case "2":
+                //        Console.WriteLine("Tirsdag");
+                //        break;
+                //    case "3":
+                //        Console.WriteLine("Onsdag");
+                //        break;
+                //    case "4":
+                //        Console.WriteLine("Torsdag");
+                //        break;
+                //    case "5":
+                //        Console.WriteLine("Fredag");
+                //        break;
+                //    case "6":
+                //        Console.WriteLine("Lørdag");
+                //        break;
+                //    case "7":
+                //        Console.WriteLine("Søndag");
+                //        break;
+                //    default:
+                //        Run();
+                //        break;
+                //}
+
+        }
+    static void Main(string[] args)
+    {
+        Program runProgram = new Program();
+        runProgram.Run();
+    }
+}
