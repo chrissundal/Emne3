62347fe3d6a144b3bdf438c2e3bd9e6fe0b114de
chrissundal <christoffersj@hotmail.com>
2024-11-17 12:07:22 +0100

    f

---
+++ Krokkodillespill/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Krokodillespill
+{
+    internal class Program
+    {
+        Random rnd = new Random();
+        public void Run()
+        {
+            int score = 0;
+            bool exit = false;
+            Console.WriteLine("Krokodillespill");
+            Console.WriteLine("Skriv inn < = >");
+            while (exit == false)
+        {
+            int firstNumber = rnd.Next(1, 11);
+            int secondNumber = rnd.Next(1, 11);
+
+            Console.WriteLine($"Du har {score} poeng\n"); 
+            Console.WriteLine(firstNumber + " _ " + secondNumber);
+
+            var input = Console.ReadLine();
+
+            if (input == "<" || input == ">" || input == "=")
+            {
+                if (firstNumber < secondNumber && input == "<")
+                {
+                    Console.WriteLine("\nRiktig");
+                    score++;
+                }
+                else if (firstNumber > secondNumber && input == ">")
+                {
+                    Console.WriteLine("\nRiktig");
+                    score++;
+                }
+                else if (firstNumber == secondNumber && input == "=")
+                {
+                    Console.WriteLine("\nRiktig");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("\nFeil");
+                    score--;
+                }
+            }
+            else
+            {
+                exit = true;
+            }
+        }
+        Console.WriteLine($"Spillet er Avsluttet. Din totalsum ble: {score}");
+        }
+        static void Main(string[] args)
+        {
+            Program runProgram = new Program();
+            runProgram.Run();
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        public void Run()
+        {
+            string text = "Terje er kul!";
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(text);
+            }
+
+            foreach (var chara in text)
+            {
+                Console.WriteLine(chara);
+            }
+            int RoundNumber = 1;
+            while (RoundNumber <= 10)
+            {
+                Console.WriteLine($"Runde nummer: {RoundNumber}");
+                RoundNumber++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program runProgram = new Program();
+            runProgram.Run();
+        }
+    }
+}
